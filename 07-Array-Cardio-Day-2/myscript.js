f391b1a93d8d59dc5b2d924017a6dba808066b77
Camilla Murgia <c.murgia4@studenti.uniss.it>
2020-12-21 18:25:57 +0100

    implemented .find method

--- 07-Array-Cardio-Day-2/myscript.js
+++ 07-Array-Cardio-Day-2/myscript.js
@@ -35,7 +35,8 @@ const allAdults = people.every(person =>((new Date()).getFullYear()) - person.ye
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
-
+const comment = comments.find(comment => comment.id === 823423);
+console.log(comment);
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
