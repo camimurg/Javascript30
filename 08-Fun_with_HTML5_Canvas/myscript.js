c32ed0eaa73340a084279ad0cdf961434305448d
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-01-11 18:02:56 +0100

    added the build-in fuctions to start up the line

--- 08-Fun_with_HTML5_Canvas/myscript.js
+++ 08-Fun_with_HTML5_Canvas/myscript.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector('#draw');
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d'); // Where we can draw
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -14,7 +14,12 @@ let lastY = 0;
 function draw(e) {
   if(!isDrawing) return; //stop the function to running when they are not moused down (drawing)
   console.log(e);
-};
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+}; // all this build-in function start up our line, but we're not going to actually see anything on the age until we stroke
+
 
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mousedown', () => isDrawing = true);
