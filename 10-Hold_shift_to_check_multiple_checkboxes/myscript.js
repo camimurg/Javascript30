0740bd72a15cfbbdbd3a1ff832f15116f536e2b6
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-01-13 23:31:57 +0100

    loop into the checkboxes, taken the shift input

--- 10-Hold_shift_to_check_multiple_checkboxes/myscript.js
+++ 10-Hold_shift_to_check_multiple_checkboxes/myscript.js
@@ -4,6 +4,19 @@ let lastChecked;
 
 function handleCheck(e) {
   // check if they had the schift key down
+  // and check that they are checking it
+  let inBetween = false
+  if (e.shiftKey && this.checked) {
+    // go ahead and do what we please
+    // loop over every single checkbox
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if(checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+    });
+  }
+
   lastChecked = this;
 }
 
