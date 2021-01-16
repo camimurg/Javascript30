735ea1928662440abecdedc9f7cad9fd02f3be10
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-01-16 15:51:39 +0100

    Excercise done with the scrub function

--- 11-Custum_HTML5_Video_Player/myscript.js
+++ 11-Custum_HTML5_Video_Player/myscript.js
@@ -32,6 +32,12 @@ function handleProgress() {
   progressBar.style.flexBasis = `${percent}%`
 }
 
+//to jump exactly where you click on the yellow bar
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime
+}
+
  // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -41,3 +47,11 @@ toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove',(e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+// FULL SCREEN MISSING
