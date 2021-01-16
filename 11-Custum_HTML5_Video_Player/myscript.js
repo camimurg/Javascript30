e2852cb3fe856678decd852393cbf513b5c6e594
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-01-16 15:42:11 +0100

    add function to handle progress bar

--- 11-Custum_HTML5_Video_Player/myscript.js
+++ 11-Custum_HTML5_Video_Player/myscript.js
@@ -20,18 +20,24 @@ function updateButton() {
 
 //data-skip
 function skip() {
- console.log(this.dataset.skip);
  video.currentTime += parseFloat(this.dataset.skip)
 }
 
 function handleRangeUpdate() {
-  console.log(this.value)
+  video[this.name] = this.value
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`
 }
 
  // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
