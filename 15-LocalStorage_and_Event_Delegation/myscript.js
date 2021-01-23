e91c01b957734c619baa920b526f04b70e9c16fb
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-01-23 16:20:13 +0100

    create the function to populate the list based on the user input

--- 15-LocalStorage_and_Event_Delegation/myscript.js
+++ 15-LocalStorage_and_Event_Delegation/myscript.js
@@ -11,7 +11,18 @@ function addItem(e) {
   };
 
   items.push(item);
+  populateList(items, itemsList);
   this.reset(); // clear the input
 }
 
-addItems.addEventListener('submit', addItem)
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates.map((plate, i) => {
+    return `
+      <li>
+        <label for="">${plate.text}</label>
+      </li>
+    `;
+  }).join('');
+}
+
+addItems.addEventListener('submit', addItem);
