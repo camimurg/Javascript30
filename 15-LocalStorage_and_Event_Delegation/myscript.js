dc595839a4835b6488e66a4f5bf2a4c56baac751
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-01-23 17:00:22 +0100

    JSON parse on local storage

--- 15-LocalStorage_and_Event_Delegation/myscript.js
+++ 15-LocalStorage_and_Event_Delegation/myscript.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
   e.preventDefault(); // prevent the loading if the page
@@ -12,6 +12,7 @@ function addItem(e) {
 
   items.push(item);
   populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
   this.reset(); // clear the input
 }
 
@@ -27,3 +28,6 @@ function populateList(plates = [], platesList) {
 }
 
 addItems.addEventListener('submit', addItem);
+
+populateList(items, itemsList);
+
