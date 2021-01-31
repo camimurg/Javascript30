8cd83ed59cada378bc363e66e8d17a0cdc206d85
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-01-31 18:04:05 +0100

    calcolated total seconds for each video

--- 18-Adding_up_Times_with_Reduce/myscript.js
+++ 18-Adding_up_Times_with_Reduce/myscript.js
@@ -1,3 +1,10 @@
 const timeNodes = Array.from(document.querySelectorAll('[data-time]')); // we need to transform it in an array from a node object
 
-const seconds = timeNodes.map(node => node.dataset.time);
+const seconds = timeNodes
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(':').map(parseFloat);
+    return (mins * 60) + secs;
+  })
+
+  // reduce return whatever you want
