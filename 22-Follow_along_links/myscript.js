9dc225b08f6f9689e1b93bf6093f899a1615ecd3
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-02-06 16:18:10 +0100

    create function to highlight the links, basic

---
+++ 22-Follow_along_links/myscript.js
@@ -0,0 +1,12 @@
+ // 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink() {
+  console.log('highlight!!');
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
