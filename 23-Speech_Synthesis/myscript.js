9e38df615214962a378bc835901913c890d71715
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-02-10 20:41:51 +0100

    excercise done

--- 23-Speech_Synthesis/myscript.js
+++ 23-Speech_Synthesis/myscript.js
@@ -12,16 +12,32 @@ msg.text = document.querySelector('[name="text"]').value;
 function populateVoices() {
   voices = this.getVoices();
   voicesDropdown.innerHTML = voices
+  // .filter(voice => voice.lang.include('en'))
   .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
   .join('');
 }
 
 function setVoice() {
   msg.value = voices.find(voice => voice.name === this.value);
+  toggle();
 }
 
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if(startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  msg[this.name] = this.value;
+  toggle();
+}
 
 // SpeechSynthesis is a global variable
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
-
 voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
+
