2bdaf66ed70b37bace74f108758f0a69cb0ff676
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-02-13 15:43:27 +0100

    add class and style of fixed-nav

---
+++ 24-Sticky_Nav/myscript.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector('#main');
+const topOfNav = nav.offsetTop;
+
+
+function fixNav(e) {
+  console.log(topOfNav, window.scrollY);
+  if(window.scrollY >= topOfNav) {
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.classList.remove('fixed-nav')
+  }
+}
+
+window.addEventListener('scroll', fixNav);
