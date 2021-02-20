026e6e18d01070dd8d8a7710d807aa0d56e4777d
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-02-20 17:48:50 +0100

    function for buttons

--- 29-Countdown_timer/myscript.js
+++ 29-Countdown_timer/myscript.js
@@ -1,8 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  // clear any wxisting timers
+  clearInterval(countdown)
+
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
@@ -32,5 +36,12 @@ function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
   const minutes = end.getMinutes();
-  endTime.textContent = `Be back at ${hour}:${minutes}`
+  endTime.textContent = `Be back at ${hour}:${minutes < 10 ? '0' : ''}${minutes}`
 }
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
