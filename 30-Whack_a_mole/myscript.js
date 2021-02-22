34b952624aea116a81d76f732a8fbc7c8977480a
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-02-22 22:11:41 +0100

    created peep function

--- 30-Whack_a_mole/myscript.js
+++ 30-Whack_a_mole/myscript.js
@@ -18,3 +18,12 @@ function randomHole(holes) {
   return hole
 }
 
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    peep();
+  }, time);
+}
